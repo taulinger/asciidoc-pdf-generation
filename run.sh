094bc1df641ba5605641d80246651397f4baecb3
Thomas Aulinger <taulinger@gmx.de>
2021-11-17 23:39:01 +0100

    delete content of dist dir only

--- run.sh
+++ run.sh
@@ -5,12 +5,12 @@ path="$(realpath $1)"
 
 fullname="${path##*/}"
 dirname="${path%/*}"
-
+basename="${fullname%.*}"
 # If the file is in the same directory with the script,
 # path likely will not include any directory separator.
 if [ "$dirname" == "$path" ]; then
   dirname="."
 fi
 
-rm -Rf ${dirname}/dist
-mvn clean generate-resources -Dasciidoctor.sourceDocumentName=${fullname} -Dasciidoctor.sourceDirectory=${dirname} -Dasciidoctor.outputDirectory=${dirname}/dist -Dasciidoctor.outputFile=${dirname}/dist/example.pdf
+rm -Rf ${dirname}/dist/*
+mvn clean generate-resources -Dasciidoctor.sourceDocumentName=${fullname} -Dasciidoctor.sourceDirectory=${dirname} -Dasciidoctor.outputDirectory=${dirname}/dist -Dasciidoctor.outputFile=${dirname}/dist/${basename}.pdf
